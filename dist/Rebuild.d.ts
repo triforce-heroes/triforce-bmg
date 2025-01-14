83e12c67f69595bb709b8608a5b0266f276d5d68
David Rodrigues <david.proweb@gmail.com>
2025-01-14 15:25:52 -0300

    chore: dependencies updated;

--- dist/Rebuild.d.ts
+++ dist/Rebuild.d.ts
@@ -1,2 +1,2 @@
 import { MessageEntry } from "./types/Message.js";
-export declare function rebuild(source: Buffer, messagesPatches: MessageEntry[]): Buffer;
+export declare function rebuild(source: Buffer, messagesPatches: MessageEntry[]): Buffer<ArrayBufferLike>;
